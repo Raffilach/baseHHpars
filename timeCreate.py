29f000d9715a69fa19e143c2708a1f036f77e68f
Zabrodivshiy Kivi <126714383+Raffilach@users.noreply.github.com>
2024-07-22 00:41:29 +0300

    Update timeCreate.py
    RU
    Переделана логика работы скрипта проверки актуальности вакансий, теперь при взгляде не конструируется ссылка из id, вместо этого при парсинге берется дополнительная ссылка которую мы раньше строили.
    EN
    The logic of the script for checking the relevance of vacancies has been reworked; now, when viewed, a link from the id is not constructed; instead, when parsing, an additional link is taken that we previously built.

--- timeCreate.py
+++ timeCreate.py
@@ -5,78 +5,95 @@ import time
 from tqdm import tqdm
 from colorama import Fore, init
 
-def check_vacancy(df, id, index):
+init(autoreset=True)  # Инициализация colorama для автоматического сброса цвета
+
+
+def check_vacancy(df, js_url, index):
+    time.sleep(0.5)
     try:
-        url = f'https://api.hh.ru/vacancies/{id}'
-        response = requests.get(url)
+        response = requests.get(js_url)
         response.raise_for_status()
 
         item = response.json()
-        actchek = item.get("type", {}).get("name", "Нет:(")
+        actchek = item.get("type", {}).get("id", "Нет:(")
 
-        if actchek == "Открытая":
+        if actchek == "open":
             df.at[index, 'status'] = "Открыта"
             df.at[index, 'check_Time'] = datetime.now().strftime('%d.%m.%Y - %H:%M:%S')
+            df.at[index, 'error_attempts'] = 0
+            df.at[index, 'error_Message'] = 0
         else:
             df.at[index, 'status'] = "Закрыта"
             df.at[index, 'closing_Date'] = datetime.now().strftime('%d.%m.%Y - %H:%M:%S')
-            df.at[index, 'closing_Year'] = int(datetime.now().year)
-            df.at[index, 'closing_Month'] = int(datetime.now().month)
-            df.at[index, 'closing_Day'] = int(datetime.now().day)
-            df.at[index, 'check_Time'] = datetime.now().strftime('%d.%м.%Y - %H:%M:%S')
+            df.at[index, 'closing_Year'] = datetime.now().year
+            df.at[index, 'closing_Month'] = datetime.now().month
+            df.at[index, 'closing_Day'] = datetime.now().day
+            df.at[index, 'check_Time'] = datetime.now().strftime('%d.%m.%Y - %H:%M:%S')
+            df.at[index, 'error_attempts'] = 0
+            df.at[index, 'error_Message'] = 0
     except requests.RequestException as e:
-        df.at[index, 'status'] = "Закрыта"
-        df.at[index, 'closing_Date'] = datetime.now().strftime('%d.%м.%Y - %H:%М:%S')
-        df.at[index, 'closing_Year'] = int(datetime.now().year)
-        df.at[index, 'closing_Month'] = int(datetime.now().month)
-        df.at[index, 'closing_Day'] = int(datetime.now().day)
-        df.at[index, 'check_Time'] = datetime.now().strftime('%d.%м.%Y - %H:%М:%S')
+        df.at[index, 'status'] = "Ошибка"
+        error_attempts = df.at[index, 'error_attempts'] + 1 if not pd.isna(df.at[index, 'error_attempts']) else 1
+        df.at[index, 'error_attempts'] = error_attempts
+        df.at[index, 'error_Message'] = str(e)
+        df.at[index, 'check_Time'] = datetime.now().strftime('%d.%m.%Y - %H:%M:%S')
+
+        if error_attempts >= 2:  # Если две проверки закончились ошибкой, закрываем вакансию
+            df.at[index, 'status'] = "Закрыта"
+            df.at[index, 'closing_Date'] = datetime.now().strftime('%d.%m.%Y - %H:%M:%S')
+            df.at[index, 'closing_Year'] = datetime.now().year
+            df.at[index, 'closing_Month'] = datetime.now().month
+            df.at[index, 'closing_Day'] = datetime.now().day
+            df.at[index, 'error_attempts'] = 2
+            df.at[index, 'error_Message'] = 0
+
+
+
+def check_columns(df):
+    columns = ['status', 'closing_Date', 'closing_Year', 'closing_Month', 'closing_Day', 'check_Time',
+               'error_Message', 'error_attempts']
+    for col in columns:
+        if col not in df.columns:
+            df[col] = None
+
 
 def main():
-    input_file_path = 'OFFresParsHH_2024-06-15.csv'
-    output_file_path = 'OFFresParsHH_2024-06-15.csv'
+    file_path = 'TEST_ParsHH_21-07-2024.csv'
 
     while True:
-        df = pd.read_csv(input_file_path)
+        df = pd.read_csv(file_path)
         print(f"{Fore.CYAN}Подключение к файлу выполнено")
 
-        if 'status' not in df.columns:
-            df['status'] = pd.NA
-        if 'closing_Date' not in df.columns:
-            df['closing_Date'] = pd.NA
-        if 'closing_Year' not in df.columns:
-            df['closing_Year'] = pd.NA
-        if 'closing_Month' not in df.columns:
-            df['closing_Month'] = pd.NA
-        if 'closing_Day' not in df.columns:
-            df['closing_Day'] = pd.NA
-        if 'check_Time' not in df.columns:
-            df['check_Time'] = pd.NA
-
-        # Приведение типов
-        df['closing_Year'] = df['closing_Year'].astype('Int64')
-        df['closing_Month'] = df['closing_Month'].astype('Int64')
-        df['closing_Day'] = df['closing_Day'].astype('Int64')
+        check_columns(df)
 
         for index, row in tqdm(df.iterrows(),
                                desc="\033[31m Проверка вакансий: \033[0m",
                                total=len(df),
                                colour='white'):
-            id = row['id']
+            js_url = row['js_url']
             status = row['status']
 
             if pd.isna(status) or status != "Закрыта":
-                check_vacancy(df, id, index)
+                check_vacancy(df, js_url, index)
 
-        df.to_csv(output_file_path, index=False)
-        print(f"{Fore.CYAN}Проверка завершена и данные сохранены в {output_file_path}")
+        print("Время")
+        df.to_csv(file_path, index=False)
+        print(f"{Fore.CYAN}Проверка завершена и данные сохранены в {file_path}")
 
         wait_time_seconds = 21600  # 6 часов в секундах
 
-        with tqdm(total=wait_time_seconds, desc="До повторной проверки", colour='green') as pbar:
+        with tqdm(total=wait_time_seconds,
+                  desc="До повторной проверки",
+                  colour='green') as pbar:
             for _ in range(wait_time_seconds):
                 time.sleep(1)
                 pbar.update(1)
 
+
 if __name__ == "__main__":
+    # ПРИМЕРНЫЕ РАСЧЕТЫ, БЕЗ УЧЕТА ЗАКРЫТЫХ ВАКАНСИЙ!!!
+    # Для более точного расчета нужна проба на практике
+    # До 86400 вакансий дни будут точными
+    # До 2500000 вакансий месяца будут точными
+    # До 30000000 вакансий года будут точными
     main()
